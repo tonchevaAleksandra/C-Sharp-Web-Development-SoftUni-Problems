918816edebe799f407c852fb763147be10336a70
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-02-20 18:49:54 +0200

    Add files

---
+++ C# Web Basic/WEB SERVER - HTTP PROTOCOL/HTTP Requester/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace HTTP_Requester
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            //TcpListener 
+        }
+
+        public static async Task HttpRequest()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "MyConsoleBrowser/1.0");
+            HttpResponseMessage response = await client.GetAsync("https://softuni.bg/");
+            //HttpResponseMessage response = await client.GetAsync("https://softuni.bg/", new FormUrlEncodedContent());
+            string result = await response.Content.ReadAsStringAsync();
+            File.WriteAllText("index.html", result);
+            //Console.WriteLine(result);
+        }
+    }
+}

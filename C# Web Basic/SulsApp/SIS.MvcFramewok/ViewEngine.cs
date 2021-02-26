5bfef816b9b240b11d9ec7835128f45f0017ba2b
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-02-26 19:58:28 +0200

    Add

--- C# Web Basic/SulsApp/SIS.MvcFramewok/ViewEngine.cs
+++ C# Web Basic/SulsApp/SIS.MvcFramewok/ViewEngine.cs
@@ -1,10 +1,62 @@
-﻿using System;
+﻿using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace SIS.MvcFramework
 {
     public class ViewEngine : IViewEngine
     {
         public string GetHtml(string templateHtml, object model)
+        {
+            var methodCode = PrepareCSharpCode(templateHtml);
+            string code = @$"using System;
+            using System.Text;
+            using System.Linq;
+            using System.Collections.Generic;
+            using SIS.MvcFramework;
+            namespace AppViewNamespace
+            {{
+            public class AppViewCode : IView
+{{
+public string GetHtml(object model)
+{{
+var html= new StringBuilder();
+{methodCode}
+return html.ToString();
+}}
+}}
+            }}";
+
+            IView view = GetInstanceFromCode(code, model);
+            string html = view.GetHtml(model);
+            return html;
+        }
+
+        private IView GetInstanceFromCode(string code, object model)
+        {
+            var compilation = CSharpCompilation.Create("AppViewAssembly")
+                  .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+                  .AddReferences(MetadataReference.CreateFromFile(typeof(IView).Assembly.Location))
+                  .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location))
+                  .AddReferences(MetadataReference.CreateFromFile(model.GetType().Assembly.Location));
+
+            var libraries = Assembly.Load(new AssemblyName("netstandart")).GetReferencedAssemblies();
+
+            foreach (var library in libraries)
+            {
+                compilation = compilation.AddReferences(MetadataReference.CreateFromFile(Assembly.Load(library).Location));
+            }
+
+            compilation = compilation.AddSyntaxTrees(SyntaxFactory.ParseSyntaxTree(code));
+
+            compilation.Emit("a.dll");
+
+            return null;
+
+        }
+
+        private string PrepareCSharpCode(string templateHtml)
         {
             return string.Empty;
         }
---
+++ C# Web Basic/SulsApp/TestConverter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace TestConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetHtml());
+        }
+
+        static string GetHtml()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("<h1>Hello</h1>");
+            if (DateTime.UtcNow.Year == 2021)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    if (i % 3 == 0)
+                    {
+                        sb.AppendLine("<p><span>" + i + "</span> <span>" + DateTime.UtcNow.Year + "</span></p>");
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}

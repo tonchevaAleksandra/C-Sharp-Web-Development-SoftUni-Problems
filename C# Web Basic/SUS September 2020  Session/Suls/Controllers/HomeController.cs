d01e038f9e3cac602e5e872296c6a4fea8c0f42c
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-03-03 16:54:51 +0200

    Finish Suls.csproj

---
+++ C# Web Basic/SUS September 2020  Session/Suls/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Suls.Services;
+using SUS.HTTP;
+using SUS.MvcFramework;
+
+namespace Suls.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProblemsService problemsService;
+
+        public HomeController(IProblemsService problemsService)
+        {
+            this.problemsService = problemsService;
+        }
+
+        [HttpGet("/")]
+        public HttpResponse Index()
+        {
+            if (this.IsUserSignedIn())
+            {
+                var viewModel = this.problemsService.GetAll();
+                return this.View(viewModel, "IndexLoggedIn");
+            }
+            else
+            {
+                return this.View();
+            }
+        }
+    }
+}

bd7d66310d6a44bfa8e4fb3ea37d3fdba0e0c3fd
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-03-09 14:25:22 +0200

    Add Database Models

--- C# Web Basic/SUS September 2020  Session/Git/Data/ApplicationDbContext.cs
+++ C# Web Basic/SUS September 2020  Session/Git/Data/ApplicationDbContext.cs
@@ -1,25 +1,44 @@
-﻿using IRunes.Models;
+﻿using Git.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace IRunes.Data
+namespace Git.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        //public DbSet<User> Users { get; set; }
-        //public DbSet<Track> Tracks { get; set; }
-        //public DbSet<Album> Albums { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Repository> Repositories { get; set; }
+        public DbSet<Commit> Commits { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer(@"Server=ALEKSANDRA\SQLEXPRESS;Database=Git;Integrated Security=True;");
+                optionsBuilder.UseSqlServer(DatabaseConfiguration.ConnectionString);
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-           
+            modelBuilder.Entity<Commit>(c =>
+            {
+                c.HasOne(x => x.Creator)
+                    .WithMany(z => z.Commits)
+                    .HasForeignKey(x => x.CreatorId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                c.HasOne(x => x.Repository)
+                    .WithMany(z => z.Commits)
+                    .HasForeignKey(x => x.RepositoryId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<Repository>(r =>
+            {
+                r.HasOne(x => x.Owner)
+                    .WithMany(z => z.Repositories)
+                    .HasForeignKey(x => x.OwnerId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
         }
     }
 }
---
+++ C# Web Basic/SUS September 2020  Session/Git/Data/DatabaseConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace Git.Data
+{
+    public class DatabaseConfiguration
+    {
+        public static string ConnectionString = @"Server=ALEKSANDRA\SQLEXPRESS;Database=Git;Integrated Security=True;";
+    }
+}

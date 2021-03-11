7438fa8fc6d89da48ab5c5361de54f9967c1c7c4
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-03-11 18:24:13 +0200

    Add Razor practice

---
+++ C# Web Basic/AspNetAppForTestingRazor/AspNetAppForTestingRazor/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using AspNetAppForTestingRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetAppForTestingRazor.Data;
+using AspNetAppForTestingRazor.ViewModels.Home;
+
+namespace AspNetAppForTestingRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext dbContext;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index(int id)
+        {
+            //// Not recommended
+            ////Dictionary<string, object>
+            //this.ViewData["Year"] = 2021;
+            //this.ViewData["Name"] = "Aleks";
+            //this.ViewData["UsersCount"] = this.dbContext.Users.Count();
+
+            //// Not recommended
+            ////dynamic
+            //this.ViewBag.Name = "Aleks";
+            //this.ViewBag.Year = "2021";
+            //this.ViewBag.UsersCount = this.dbContext.Users.Count();
+            //this.ViewBag.Calc = new Func<int>(() => 3);
+            //this.ViewBag.Calc();
+
+            var viewModel = new IndexViewModel()
+            {
+                Id = id,
+                Name = "Aleks",
+                Year = DateTime.UtcNow.Year,
+                UsersCount = this.dbContext.Users.Count(),
+                ProcessorsCount = Environment.ProcessorCount
+            };
+
+            return View(viewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ C# Web Basic/AspNetAppForTestingRazor/AspNetAppForTestingRazor/ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetAppForTestingRazor.ViewModels.Home
+{
+    public class IndexViewModel
+    {
+        public int Year { get; set; }
+        public int UsersCount { get; set; }
+        public int ProcessorsCount { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

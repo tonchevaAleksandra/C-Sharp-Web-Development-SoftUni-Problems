6b98ec2cbbd7836504071424e9bbb5f56190cb3a
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-03-12 15:35:28 +0200

    Add

---
+++ ASP.NET Core/AspNetAppForTestingRazor/AspNetAppForTestingRazor/TagHelper/BootstrapTableTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AspNetAppForTestingRazor.TagHelper
+{
+    [HtmlTargetElement("table", Attributes = "bootstrap-table")]
+    public class BootstrapTableTagHelper : Microsoft.AspNetCore.Razor.TagHelpers.TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll("bootstrap-table");
+            output.Attributes.Add(new TagHelperAttribute("class", "table table-striped table-hover table-sm"));
+        }
+    }
+}
--- ASP.NET Core/AspNetAppForTestingRazor/AspNetAppForTestingRazor/ViewModels/Home/IndexViewModel.cs
+++ ASP.NET Core/AspNetAppForTestingRazor/AspNetAppForTestingRazor/ViewModels/Home/IndexViewModel.cs
@@ -1,12 +1,18 @@
-﻿namespace AspNetAppForTestingRazor.ViewModels.Home
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetAppForTestingRazor.ViewModels.Home
 {
     public class IndexViewModel
     {
+        [Range(2000,2050)]
         public int Year { get; set; }
         public int UsersCount { get; set; }
         public int ProcessorsCount { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
     }
 }

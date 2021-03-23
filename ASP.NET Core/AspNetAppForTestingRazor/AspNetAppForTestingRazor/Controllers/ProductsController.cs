9ab7ff922c3ea4fc3d224f61d94c9b3cb0780daf
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-03-23 18:24:59 +0200

    Add

---
+++ ASP.NET Core/AspNetAppForTestingRazor/AspNetAppForTestingRazor/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using AspNetAppForTestingRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AspNetAppForTestingRazor.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public Product Test()
+        {
+            return new Product()
+            {
+                ActiveFrom = DateTime.UtcNow,
+                Description = "description",
+                Price = 123.45,
+                Id = 123,
+                Name = "name"
+            };
+        }
+        [HttpDelete]
+        public string SomeMethod()
+        {
+            return "DELETE";
+        }
+    }
+}

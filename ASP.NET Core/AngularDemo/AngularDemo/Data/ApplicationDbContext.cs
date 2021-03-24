753571d947e4e1cf77140aae5a89cd7f3921b475
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-03-24 13:13:03 +0200

    Add

---
+++ ASP.NET Core/AngularDemo/AngularDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AngularDemo.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularDemo.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}

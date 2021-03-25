ce2ac6781665b88c0432e8e686fa821c5f0fa07d
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-03-25 18:49:56 +0200

    Add

--- ASP.NET Core/AspNetAppForTestingRazor/AspNetAppForTestingRazor/Pages/ContactForm.cshtml.cs
+++ ASP.NET Core/AspNetAppForTestingRazor/AspNetAppForTestingRazor/Pages/ContactForm.cshtml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspNetAppForTestingRazor.Pages
@@ -44,5 +45,30 @@ namespace AspNetAppForTestingRazor.Pages
             return this.Redirect("/");
             
         }
+
+        public override void OnPageHandlerSelected (PageHandlerSelectedContext context)
+        {
+            base.OnPageHandlerSelected(context);
+        } // After a handler (OnGet, OnPost) has been selected,before model binding
+
+        public override void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+        {
+            base.OnPageHandlerExecuting(context);
+        } // Before the handler executes, after model binding
+
+        public override void OnPageHandlerExecuted(PageHandlerExecutedContext context)
+        {
+            base.OnPageHandlerExecuted(context);
+        } // After the handler executes, before the action result
+
+        public override Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+        {
+            //if (....)
+            //{
+            //var result = await next();
+            //}
+
+            return base.OnPageHandlerExecutionAsync(context, next);
+        } // Before the handler is invoked, after model binding
     }
 }
